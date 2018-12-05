56d7764a08b3a19c9fc2d6e8899ac930354eaebd
Shawn Gilroy <shawn.gilroy@temple.edu>
2018-12-05 16:23:07 -0600

    fix lint

--- src/components/pages/Tutorials.js
+++ src/components/pages/Tutorials.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import tutorialData from '../../json/tutorials.json';
 
-function checkParamsPresent()
-{
-	return document.location.toString().indexOf('?') !== -1;
+function checkParamsPresent() {
+    return document.location.toString().indexOf( '?' ) !== -1;
 }
 
 class Tutorials extends Component {
@@ -12,17 +11,16 @@ class Tutorials extends Component {
 
         var relevantTutorial = tutorialData.Tutorials;
 
-        if (isInDirectory)
-        {
-            const tutorialLinks = relevantTutorial.map((tutorial) => {
+        if ( isInDirectory ) {
+            const tutorialLinks = relevantTutorial.map( ( tutorial ) => {
                 return (
-                    <a href= { 'Tutorials/?index=' + tutorial.Index }
-                       className = "lead"
-                       key= { tutorial.Index }>
-                       { tutorial.Title }
-                       <br/><br/></a>
+                    <a href={ 'Tutorials/?index=' + tutorial.Index }
+                        className="lead"
+                        key={ tutorial.Index }>
+                        { tutorial.Title }
+                        <br /><br /></a>
                 )
-            });
+            } );
 
             return (
                 <div className="container">
@@ -30,43 +28,38 @@ class Tutorials extends Component {
                         <div className="col-md-12">
                             <div className="jumbotron">
                                 <h2 id="titleRef" className="h1-responsive">SQAB Pre-eminent Tutorials</h2>
-                                <br/>
+                                <br />
                                 <p id="descriptionRef" className="lead">SQAB is committed to simplifying the transition to quantitative behavior analysis for students as well as advanced researchers. These videos of presentations from leaders in the field are at various levels and are appropriate for individual, classroom, and seminar use. Individual video presentations can be accessed by clicking on the appropriate links below. All of the videos can be accessed on our <a href='https://www.youtube.com/c/SocietyfortheQuantitativeAnalysesofBehavior'>YouTube</a> channel.</p>
                             </div>
                         </div>
                     </div>
-                    
+
                     <div className="row">
                         <div className="col-md-12">
                             <div id="tutorialDirectory" className="well">
-                            {
-                                tutorialLinks
-                            }
+                                {
+                                    tutorialLinks
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
             )
         }
-        else
-        {
-            var paramString = document.location.toString().split("?")[1];
-            var index = parseInt(paramString.split("=")[1]);
+        else {
+            var paramString = document.location.toString().split( "?" )[ 1 ];
+            var index = parseInt( paramString.split( "=" )[ 1 ] );
 
-            const buttonPrev = (index === 0) ?
-                <a id="buttonPrev" 
-                   href="#" 
-                   className="btn btn-info btn-raised invisible">Previous Video</a> :
-                <a id="buttonPrev" 
-                   href= { "?index=" + parseInt(index - 1) }
-                   className="btn btn-info btn-raised">Previous Video</a>;
+            const buttonPrev = ( index === 0 ) ?
+                <></> :
+                <a id="buttonPrev"
+                    href={ "?index=" + parseInt( index - 1 ) }
+                    className="btn btn-info btn-raised">Previous Video</a>;
 
-            const buttonNext = (index >= relevantTutorial.length - 1) ?
-                <a id="buttonNext" 
-                    href="#" 
-                    className="btn btn-info btn-raised invisible">Next Video</a> :
-                <a id="buttonNext" 
-                    href= { "?index=" + parseInt(index + 1) }
+            const buttonNext = ( index >= relevantTutorial.length - 1 ) ?
+                <></> :
+                <a id="buttonNext"
+                    href={ "?index=" + parseInt( index + 1 ) }
                     className="btn btn-info btn-raised pull-right">Next Video</a>;
 
             return (
@@ -74,22 +67,22 @@ class Tutorials extends Component {
                     <div className="row">
                         <div className="col-md-12">
                             <div className="jumbotron">
-                                <h2 id="titleRef" className="h1-responsive">{ relevantTutorial[index].Title }</h2>
-                                <br/>
-                                <p id="descriptionRef" className="lead">{ relevantTutorial[index].Summary }</p>
+                                <h2 id="titleRef" className="h1-responsive">{ relevantTutorial[ index ].Title }</h2>
+                                <br />
+                                <p id="descriptionRef" className="lead">{ relevantTutorial[ index ].Summary }</p>
                             </div>
                         </div>
                     </div>
-                    
+
                     <div className="row">
-                        <div className="col-md-12">                       
+                        <div className="col-md-12">
                             <div id="tutorialChartDiv" className="well">
                                 <div id="tutorialIframeDiv" className="embed-responsive embed-responsive-16by9 text-center">
-                                    <iframe src = { relevantTutorial[index].Video }/>
+                                    <iframe title="tutorialVideo" src={ relevantTutorial[ index ].Video } />
                                 </div>
-                        
-                                <hr className="extra-margin"/>
-                        
+
+                                <hr className="extra-margin" />
+
                                 <div className="read-more">
                                     { buttonPrev }
                                     { buttonNext }
